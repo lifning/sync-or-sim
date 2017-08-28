2ff112866fae878cf49135eae469bc6a700b2cfa
Vivian Lim <vivvnlim@gmail.com>
2017-08-27 19:08:50 -0700

    Try to read bytes every step for the player name (in pkmn blue)

--- brains/andalite.py
+++ brains/andalite.py
@@ -1,4 +1,16 @@
+import pprint
 from sapiens import Sapiens
+
+def ReadBytesInRange(game, startOffset, endOffset):
+    if endOffset - startOffset <= 0:
+        return None
+
+    b = {}
+
+    for offset in range(startOffset, endOffset + 1):
+        b[offset] = game._Byte(offset)
+
+    return b
 
 class Andalite(Sapiens):
     name = 'andalite'
@@ -7,6 +19,7 @@ class Andalite(Sapiens):
         Sapiens.__init__(self, game, args)
 
     def Step(self):
+        pprint.pprint(ReadBytesInRange(self.game, 0xa598, 0xa5a2))
         return Sapiens.Step(self)
 
     def Event(self, evt):
