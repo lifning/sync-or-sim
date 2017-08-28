883c2dd5b1f0dde022e9c4e0edf56dc532ad8bd1
Vivian Lim <vivvnlim@gmail.com>
2017-08-27 19:31:38 -0700

    Add funciton to superopti which takes two address and attempts to read from wram

--- brains/andalite.py
+++ brains/andalite.py
@@ -1,17 +1,6 @@
 import pprint
 from sapiens import Sapiens
 
-def ReadBytesInRange(game, startOffset, endOffset):
-    if endOffset - startOffset <= 0:
-        return None
-
-    b = {}
-
-    for offset in range(startOffset, endOffset + 1):
-        b[offset] = game._Byte(offset)
-
-    return b
-
 class Andalite(Sapiens):
     name = 'andalite'
 
@@ -19,7 +8,7 @@ class Andalite(Sapiens):
         Sapiens.__init__(self, game, args)
 
     def Step(self):
-        pprint.pprint(ReadBytesInRange(self.game, 0xa598, 0xa5a2))
+        pprint.pprint(self.game.ReadBytesInRange(0xcc24, 0xcc35)) # menu data
         return Sapiens.Step(self)
 
     def Event(self, evt):
